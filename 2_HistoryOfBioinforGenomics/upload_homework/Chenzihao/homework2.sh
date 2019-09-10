6ba3cb7c9519a07bc961f9dd7bc570b72571f330
genometree <genometree@gmail.com>
2019-09-10 16:27:24 +0800

    CZH_Homework2_of_genome_info

--- 2_HistoryOfBioinforGenomics/upload_homework/Chenzihao/homework2.sh
+++ 2_HistoryOfBioinforGenomics/upload_homework/Chenzihao/homework2.sh
@@ -9,16 +9,20 @@
 source /etc/profile.d/modules.sh
 
 #download  fastafile
-#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/493/695/GCF_000493695.1_BalAcu1.0/GCF_000493695.1_BalAcu1.0_genomic.fna.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/493/695/GCF_000493695.1_BalAcu1.0/GCF_000493695.1_BalAcu1.0_genomic.fna.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/balAcu1/bigZips/balAcu1.fa.gz
 
 #get the information of genome from fastafile
-#gzip -d GCF_000493695.1_BalAcu1.0_genomic.fna.gz
+gzip -d gzip -d balAcu1.fa.gz
+
 #module load samtools 
-#samtools faidx GCF_000493695.1_BalAcu1.0_genomic.fna
-size=`tail -1 /public/home/Chenzh275/Data/balAcu1/GCF_000493695.1_BalAcu1.0_genomic.fna.fai|cut -f 3`
+samtools faidx GCF_000493695.1_BalAcu1.0_genomic.fna
+samtools faidx balAcu1.fa
+
+size=`tail -1 /public/home/Chenzh275/Data/balAcu1/balAcu1.fa.fai |cut -f 3`
 printf " Genome Size:	$size\n"  
 
-#I can't find the chromosome number from fna-format file, maybe i'll update this after the fa-format file downloaded.
+#there is no Minke Whales' chromosome information.
 #ChrmNum=
 #printf " Chromosome number:	"$ChrmNum
 
